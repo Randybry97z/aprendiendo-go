21352f10df83a550dab260afd2a79f2b71ee0807
Bryan <bssandovalrod@gmail.com>
2020-08-11 14:54:27 -0500

    Slice pt2

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	/*
 		//ARRAYS
@@ -27,25 +25,42 @@ func main() {
 	*/
 
 	/*
-		//SLICE (se puede aumentar o disminuir su longitud)
+			//SLICE (se puede aumentar o disminuir su longitud)
 
+			numeros := []int{1, 2, 3, 4}
+
+			numeros = append(numeros, 5)
+			numeros = append(numeros, 6)
+			numeros = append(numeros, 7)
+			numeros = append(numeros, 8)
+		numeros = append(numeros, 9)
+		numeros = append(numeros, 10)
+
+		nuevoSlice := numeros[0:5]
+		tercerSlice := numeros[0:3]
+
+		numeros[0] = 100
+
+		fmt.Println(numeros)
+		fmt.Println(nuevoSlice)
+		fmt.Println(tercerSlice)
+
+		meses := []string{"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre"}
+		//Se debe tener en cuenta: Un puntero Una longitud Una capacidad
+
+		longitud := len(meses)
+		capacidad := cap(meses)
+
+		fmt.Printf("La longitud es: %v, la capacidad es: %v %p\n", longitud, capacidad, meses)
+
+		meses = append(meses, "Octubre") //Si se encuentra en capacidad máxima se genera un nuevo arreglo
+
+		meses = append(meses, "Noviembre")
+		meses = append(meses, "Diciembre")
+
+		longitud = len(meses)
+		capacidad = cap(meses)
+
+		fmt.Printf("La longitud es: %v, la capacidad es: %v %p\n", longitud, capacidad, meses)
 	*/
-	numeros := []int{1, 2, 3, 4}
-
-	numeros = append(numeros, 5)
-	numeros = append(numeros, 6)
-	numeros = append(numeros, 7)
-	numeros = append(numeros, 8)
-	numeros = append(numeros, 9)
-	numeros = append(numeros, 10)
-
-	nuevoSlice := numeros[0:5]
-	tercerSlice := numeros[0:3]
-
-	numeros[0] = 100
-
-	fmt.Println(numeros)
-	fmt.Println(nuevoSlice)
-	fmt.Println(tercerSlice)
-
 }
