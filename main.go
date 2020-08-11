c0b99616262ac9d1f19077ecdbf9e60fa53aa577
Bryan <bssandovalrod@gmail.com>
2020-08-11 10:49:07 -0500

    Diferentes formas de asignar

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import "fmt"
 //Exec go build (compila y genera ejecutable) || go run compila internamente
 
 func main() {
-	var nombre string
-	fmt.Println(nombre)
+	var name string // default ==  ""
+	var age int     // default == 0
+	var lastName string = "Sandoval"
+
+	name = "Bryan Saúl"
+	age = 23
+
+	city := "México"
+	postalCode := 55635
+
+	var height = 1.73
+
+	fmt.Println(name, lastName)
+	fmt.Println(age, height)
+	fmt.Println(city, postalCode)
 }
