1cf24b2cdee8b4500f153d730f54c5acde84bcdc
Bryan <bssandovalrod@gmail.com>
2020-08-11 11:18:52 -0500

    Constantes y operadores

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
-
 //Exec go build (compila y genera ejecutable) || go run compila internamente
+
+//CONSTANTES
+
+//const COURSE string = "Curso profesional de Go!"
 
 func main() {
 
@@ -31,9 +33,33 @@ func main() {
 	*/
 
 	//DECLARACIÓN DE MÚLTIPLES VARIABLES
-	//var name, lastName, country string
+	/* //var name, lastName, country string
 	//var name, lastName, country = "Bryan", "Sandoval", "México"
 	name, lastName, country := "Bryan", "Sandoval", "México"
 
-	fmt.Println(name, lastName, country)
+	fmt.Println(name, lastName, country) */
+	//fmt.Println(COURSE)
+
+	/*
+			OPERADORES RELACIONALES
+			>
+			<
+			>=
+			<=
+			==
+			!=
+
+		var edad = 10
+		var resultado = edad > 5
+
+		fmt.Println(resultado)
+	*/
+
+	/* //OPERADORES LÓGICOS (&& ; || ; !)
+
+	//resultado := 20 == 20 && 30 == 30
+	//resultado := 20 > 40 && 30 == 30
+	//resultado := 20 == 20 || 30 == 30 || 20 > 40
+	resultado:= 15 == 15 && 60 < 100 && (90 < 100 || 100 == 90)
+	fmt.Println(resultado) */
 }
