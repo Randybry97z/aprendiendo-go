8c287742e0d7e907ab0bb680df202a3f718fa2b4
Bryan <bssandovalrod@gmail.com>
2020-08-17 12:31:45 -0500

    switch y valores de un mapa

---
+++ condicionales.go
@@ -0,0 +1,49 @@
+package condicionales
+
+func condicionales() {
+	//CONDICIONALES
+
+	/*
+		//if-else
+		if false {
+			fmt.Println("La condicion se cumple")
+		} else {
+			fmt.Println("La condicion no se cumple")
+		}
+
+		var edad int
+		fmt.Print("Ingresa tu edad: ")
+		fmt.Scanf("%d", &edad)
+
+		if edad >= 18 {
+			fmt.Println("Eres mayor de edad")
+		} else {
+			fmt.Println("Eres menor de edad")
+		}
+	*/
+
+	/* //else if
+	var calificacion int
+	fmt.Println("Ingresa tu calificación: ")
+	fmt.Scanf("%d", &calificacion)
+
+	if calificacion == 10 {
+		fmt.Println("Tu calificación es perfecta")
+	} else if calificacion == 8 || calificacion == 9 {
+		fmt.Println("Tu calificación es excelente")
+	} else if calificacion == 6 || calificacion == 7 {
+		fmt.Println("Tu calificación es aprobatoria")
+	} else if calificacion >= 0 && calificacion <= 5 {
+		fmt.Println("Reprobaste")
+	} else {
+		fmt.Println("Calificación no válida")
+	} */
+
+	/* //DECLARANDO VARIABLES EN CONDICIONES
+	if nombre, edad := "Cody", 7; nombre == "Cody " {
+		fmt.Println("Hola ", nombre, "bienvenido al curso de Go")
+	} else {
+		fmt.Println("Los valores son: ", nombre, edad)
+	} */
+
+}
--- main.go
+++ main.go
@@ -3,48 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	//CONDICIONALES
-
 	/*
-		//if-else
-		if false {
-			fmt.Println("La condicion se cumple")
-		} else {
-			fmt.Println("La condicion no se cumple")
-		}
+		//SWITCH
 
-		var edad int
-		fmt.Print("Ingresa tu edad: ")
-		fmt.Scanf("%d", &edad)
+		var calificacion int
 
-		if edad >= 18 {
-			fmt.Println("Eres mayor de edad")
-		} else {
-			fmt.Println("Eres menor de edad")
-		}
-	*/
+		fmt.Println("Ingresa una calificación: ")
+		fmt.Scanf("%d", &calificacion)
 
-	/* //else if
-	var calificacion int
-	fmt.Println("Ingresa tu calificación: ")
-	fmt.Scanf("%d", &calificacion)
-
-	if calificacion == 10 {
-		fmt.Println("Tu calificación es perfecta")
-	} else if calificacion == 8 || calificacion == 9 {
-		fmt.Println("Tu calificación es excelente")
-	} else if calificacion == 6 || calificacion == 7 {
-		fmt.Println("Tu calificación es aprobatoria")
-	} else if calificacion >= 0 && calificacion <= 5 {
-		fmt.Println("Reprobaste")
-	} else {
+		/* switch {
+		case calificacion == 10:
+			fmt.Println("Calificacion perfecta")
+		case calificacion == 8 || calificacion == 9:
+			fmt.Println("Calificación excelente")
+		case calificacion == 6 || calificacion == 7:
+			fmt.Println("Calificación aprobatoria")
+		case calificacion >= 0 && calificacion <= 5:
+			fmt.Println("Reprobado")
+		default:
+			fmt.Println("Calificación no válida")
+		} */
+	/* switch calificacion{
+	case calificacion 10:
+		fmt.Println("Calificacion perfecta")
+	case calificacion 8,9:
+		fmt.Println("Calificación excelente")
+	case calificacion 6,7:
+		fmt.Println("Calificación aprobatoria")
+	case 0,1,2,3,4,5:
+		fmt.Println("Reprobado")
+	default:
 		fmt.Println("Calificación no válida")
 	} */
 
-	if nombre, edad := "Cody", 7; nombre == "Cody " {
-		fmt.Println("Hola ", nombre, "bienvenido al curso de Go")
-	} else {
-		fmt.Println("Los valores son: ", nombre, edad)
-	}
+	//OBTENER VALORES DE UN MAPA
 
+	usuarios := make(map[string]string)
+
+	usuarios["bryan"] = "bssandovalrod@gmail.com"
+
+	//Un valor de un mapa retorna el valor y un valor booleano
+	correo, ok := usuarios["bryan"]
+
+	if ok {
+		fmt.Println(correo)
+	} else {
+		fmt.Println("No fue posible obtener el valor")
+	}
 }
