cd66d396212d050a692421790e8350a6ee3f28f9
Bryan <bssandovalrod@gmail.com>
2020-08-17 12:40:56 -0500

    for y switch

---
+++ control.go
@@ -0,0 +1,51 @@
+package control
+
+func control() {
+	/*
+		//SWITCH
+
+		var calificacion int
+
+		fmt.Println("Ingresa una calificación: ")
+		fmt.Scanf("%d", &calificacion)
+
+		/* switch {
+		case calificacion == 10:
+			fmt.Println("Calificacion perfecta")
+		case calificacion == 8 || calificacion == 9:
+			fmt.Println("Calificación excelente")
+		case calificacion == 6 || calificacion == 7:
+			fmt.Println("Calificación aprobatoria")
+		case calificacion >= 0 && calificacion <= 5:
+			fmt.Println("Reprobado")
+		default:
+			fmt.Println("Calificación no válida")
+		} */
+	/* switch calificacion{
+	case calificacion 10:
+		fmt.Println("Calificacion perfecta")
+	case calificacion 8,9:
+		fmt.Println("Calificación excelente")
+	case calificacion 6,7:
+		fmt.Println("Calificación aprobatoria")
+	case 0,1,2,3,4,5:
+		fmt.Println("Reprobado")
+	default:
+		fmt.Println("Calificación no válida")
+	} */
+
+	/* //OBTENER VALORES DE UN MAPA
+
+	usuarios := make(map[string]string)
+
+	usuarios["bryan"] = "bssandovalrod@gmail.com"
+
+	//Un valor de un mapa retorna el valor y un valor booleano
+	correo, ok := usuarios["bryan"]
+
+	if ok {
+		fmt.Println(correo)
+	} else {
+		fmt.Println("No fue posible obtener el valor")
+	} */
+}
--- main.go
+++ main.go
@@ -1,53 +1,23 @@
 package main
 
-import "fmt"
-
 func main() {
-	/*
-		//SWITCH
+	/* //FOR
 
-		var calificacion int
-
-		fmt.Println("Ingresa una calificación: ")
-		fmt.Scanf("%d", &calificacion)
-
-		/* switch {
-		case calificacion == 10:
-			fmt.Println("Calificacion perfecta")
-		case calificacion == 8 || calificacion == 9:
-			fmt.Println("Calificación excelente")
-		case calificacion == 6 || calificacion == 7:
-			fmt.Println("Calificación aprobatoria")
-		case calificacion >= 0 && calificacion <= 5:
-			fmt.Println("Reprobado")
-		default:
-			fmt.Println("Calificación no válida")
-		} */
-	/* switch calificacion{
-	case calificacion 10:
-		fmt.Println("Calificacion perfecta")
-	case calificacion 8,9:
-		fmt.Println("Calificación excelente")
-	case calificacion 6,7:
-		fmt.Println("Calificación aprobatoria")
-	case 0,1,2,3,4,5:
-		fmt.Println("Reprobado")
-	default:
-		fmt.Println("Calificación no válida")
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 	} */
 
-	//OBTENER VALORES DE UN MAPA
+	/* //WHILE (no existe pero se reemplaza con un for)
 
-	usuarios := make(map[string]string)
-
-	usuarios["bryan"] = "bssandovalrod@gmail.com"
-
-	//Un valor de un mapa retorna el valor y un valor booleano
-	correo, ok := usuarios["bryan"]
-
-	if ok {
-		fmt.Println(correo)
-	} else {
-		fmt.Println("No fue posible obtener el valor")
+	numero := 12875
+	contador := 0
+	//while
+	for numero > 0 {
+		numero = numero / 10
+		contador++
 	}
+
+	fmt.Println("La cantidad de digitos es: ", contador) */
 }
