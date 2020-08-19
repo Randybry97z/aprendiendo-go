b505786b247d719ebf601dd637d7e20e120d9a15
Bryan <bssandovalrod@gmail.com>
2020-08-19 17:09:04 -0500

    funciones recursivas

---
+++ recursivasPunteros.go
@@ -0,0 +1,34 @@
+package recursivasPunteros
+
+/* //PUNTEROS
+func modificarVariable(param *string) {
+
+	*param = "Cambio de valor"
+
+} */
+
+/* //FUNCIONES RECURSIVAS
+func factorial(num int) int {
+
+	if num == 1 {
+		return 1
+	}
+
+	return factorial(num-1) * num
+} */
+
+func recursivasPunteros() {
+	/* //Punteros
+
+	var curso = "Curso profesional de Go!"
+	fmt.Println("Antes de la función: ", curso)
+
+	modificarVariable(&curso) //referencia
+
+	fmt.Println("Después de la función: ", curso) */
+
+	/* //Función recursiva
+	res := factorial(3)
+	fmt.Println("El factorial es: ", res) */
+
+}
