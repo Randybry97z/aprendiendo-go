ebf0f5610eaa7c1ea880e3f1b261c5773d0604c0
Bryan <bssandovalrod@gmail.com>
2020-08-19 16:36:27 -0500

    Retorno de funciones, bloques y alcances

---
+++ funciones1.go
@@ -0,0 +1,60 @@
+package funciones1
+
+/*
+//DEFINICIÓN DE FUNCIONES
+
+func saludo(user string) {
+	fmt.Println("Hola ", user)
+}
+
+func add(numero1, numero2 int) int {
+	return numero1 + numero2
+}
+
+//Retornar diferentes tipos en funcion, se definen entre paréntesis
+func add(numero1, numero2 int) (int, string) {
+	return numero1 + numero2, "Un mensaje desde suma"
+} */
+
+/* //FUNCIONES POR ARGUMENTO
+
+//Creación de tipo de dato
+type Operacion func(balance, cantidad int) int
+
+func suma(num1, num2 int) int { //Función de tipo Operacion
+	return num1 + num2
+}
+
+func resta(num1, num2 int) int { //Función de tipo Operacion
+	return num1 - num2
+}
+
+func ejecutarOperacion(funcion Operacion, balance, cantidad int) {
+	fmt.Println("Antes de la operación")
+
+	result := funcion(balance, cantidad)
+	fmt.Println("El resultado es: ", result)
+
+	fmt.Println("Despues de la operación")
+} */
+
+func main() {
+
+	//	result, message := add(2, 5)
+	//	fmt.Println(result, message)
+
+	/* 	//FUNCIÓN ANÓNIMA
+	func() {
+		fmt.Println("Hola baby")
+	}()
+
+	myFunc := func(user string) string {
+		message := fmt.Sprintf("Hola %s, te saludamos desde función sin nombre", user)
+		return message
+	}
+
+	finalMessage := myFunc("Saúl")
+		 fmt.Println(finalMessage) */
+	//ejecutarOperacion(suma, 10, 20)
+
+}
--- main.go
+++ main.go
@@ -2,61 +2,46 @@ package main
 
 import "fmt"
 
-/*
-//DEFINICIÓN DE FUNCIONES
+/* //RETORNAR FUNCIONES
 
-func saludo(user string) {
-	fmt.Println("Hola ", user)
-}
-
-func add(numero1, numero2 int) int {
-	return numero1 + numero2
-}
-
-//Retornar diferentes tipos en funcion, se definen entre paréntesis
-func add(numero1, numero2 int) (int, string) {
-	return numero1 + numero2, "Un mensaje desde suma"
-} */
-
-//FUNCIONES POR ARGUMENTO
-
-//Creación de tipo de dato
 type Operacion func(balance, cantidad int) int
 
-func suma(num1, num2 int) int { //Función de tipo Operacion
-	return num1 + num2
-}
+func crearOperacion(tipo string) Operacion {
 
-func resta(num1, num2 int) int { //Función de tipo Operacion
-	return num1 - num2
-}
-
-func ejecutarOperacion(funcion Operacion, balance, cantidad int) {
-	fmt.Println("Antes de la operación")
-
-	result := funcion(balance, cantidad)
-	fmt.Println("El resultado es: ", result)
-
-	fmt.Println("Despues de la operación")
-}
-
-func main() {
-
-	//	result, message := add(2, 5)
-	//	fmt.Println(result, message)
-
-	/* 	//FUNCIÓN ANÓNIMA
-	func() {
-		fmt.Println("Hola baby")
-	}()
-
-	myFunc := func(user string) string {
-		message := fmt.Sprintf("Hola %s, te saludamos desde función sin nombre", user)
-		return message
+	if tipo == "suma" {
+		return func(balance, cantidad int) int { return balance + cantidad }
+	} else if tipo == "resta" {
+		return func(balance, cantidad int) int { return balance - cantidad }
+	} else {
+		return func(balance, cantidad int) int { return balance * cantidad }
 	}
 
-	finalMessage := myFunc("Saúl")
-		 fmt.Println(finalMessage) */
-	ejecutarOperacion(suma, 10, 20)
+} */
+
+func main() { //Bloque 1
+
+	/* //Retornar funciones
+	suma := crearOperacion("suma")
+
+	resultado := suma(40, 50)
+
+	fmt.Println("El resultado es: ", resultado) */
+
+	//BLOQUES Y ALCANCES
+	//Una variable sólo vive en su bloque
+	var curso = "Curso de Go!"
+	var version = 10
+
+	fmt.Println(curso)
+	fmt.Println(version, "\n")
+
+	{ //Bloque 2
+		var version = 5
+
+		fmt.Println("Bloque 2")
+		fmt.Println(curso)
+		fmt.Println(version)
+
+	}
 
 }
